1869a07ee41bf436d2d735068bacb3527a9c3470
Vigneshkumar <vigneshzx@gmail.com>
2023-12-06 13:04:24 +0530

    Initial Commit

---
+++ EarningView/EarningViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarningViewApp.swift
+//  EarningView
+//
+//  Created by vignesh kumar c on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EarningViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
